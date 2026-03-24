a1112773a446281356485b456fff54d18dfedf54
zekailin00 <zekailin00@gmail.com>
2026-03-24 12:43:27 -0700

    alpha test model

--- rendering/.adamas/asset-type.d.ts
+++ rendering/.adamas/asset-type.d.ts
@@ -3,3 +3,83 @@ declare module "adamasvr:editor" {
 	import { UUID } from "crypto";
 	export const projectBundle: import("@adamasvr/sdk").ProjectBundle;
 }
+declare module "adamasvr:assets/AlphaBlendModeTest/GreenArrows.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/DecalBlend.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/DecalOpaque.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/TestCutoffDefault.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/TestBlend.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/TestCutoff25.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/TestCutoff75.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/TestOpaque.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/MatOpaque.amat" {
+	const value: import("@adamasvr/sdk").MaterialAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/MatCutoffDefault.amat" {
+	const value: import("@adamasvr/sdk").MaterialAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/MatBlend.amat" {
+	const value: import("@adamasvr/sdk").MaterialAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/MatCutoff75.amat" {
+	const value: import("@adamasvr/sdk").MaterialAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/Bed.amesh" {
+	const value: import("@adamasvr/sdk").MeshAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/MatBed.amat" {
+	const value: import("@adamasvr/sdk").MaterialAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/MatCutoff25.amat" {
+	const value: import("@adamasvr/sdk").MaterialAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/texture.atex" {
+	const value: import("@adamasvr/sdk").TextureAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/texture (1).atex" {
+	const value: import("@adamasvr/sdk").TextureAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/texture (2).atex" {
+	const value: import("@adamasvr/sdk").TextureAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/AlphaBlendModeTest.afab" {
+	const value: import("@adamasvr/sdk").PrefabAsset;
+	export default value;
+}
+declare module "adamasvr:assets/AlphaBlendModeTest/texture (3).atex" {
+	const value: import("@adamasvr/sdk").TextureAsset;
+	export default value;
+}
--- rendering/.adamas/generated.d.ts
+++ rendering/.adamas/generated.d.ts
@@ -4,7 +4,38 @@ import "./asset-type"
 
 declare module "@adamasvr/sdk"{
 export interface SceneGraph
-{}
+{
+    "@AlphaBlendModeTest": {
+        entityId: Entity,
+        "@DecalOpaque": {
+            entityId: Entity
+        },
+        "@TestBlend": {
+            entityId: Entity
+        },
+        "@TestOpaque": {
+            entityId: Entity
+        },
+        "@GreenArrows": {
+            entityId: Entity
+        },
+        "@DecalBlend": {
+            entityId: Entity
+        },
+        "@TestCutoff75": {
+            entityId: Entity
+        },
+        "@TestCutoffDefault": {
+            entityId: Entity
+        },
+        "@TestCutoff25": {
+            entityId: Entity
+        },
+        "@Bed": {
+            entityId: Entity
+        }
+    }
+}
 }
 
 export {}
