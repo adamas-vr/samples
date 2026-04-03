046daf43e561fb566022268dcc1e586c55b1cb09
zekailin00 <zekailin00@gmail.com>
2026-04-03 14:57:35 -0700

    device done

--- device/index.ts
+++ device/index.ts
@@ -10,16 +10,30 @@ import {
 	User,
 } from "@adamasvr/sdk";
 import { projectBundle } from "adamasvr:editor";
-import { quat, vec2, vec4 } from "gl-matrix";
+import { quat, vec2, vec3, vec4 } from "gl-matrix";
 
 const ACTIVE_COLOR = vec4.fromValues(1, 0, 0, 1);
 const BUTTON_ACTIVE_THRESHOLD = 0.5;
 const THUMBSTICK_MAX_ANGLE_DEGREES = 18;
+const LEFT_BUMPER_MAX_X_OFFSET = -0.003;
+const RIGHT_BUMPER_MAX_X_OFFSET = 0.003;
+const TRIGGER_MAX_X_DEGREES = -15;
 
 type ButtonVisualizer = {
 	entity: Entity;
 	defaultColor: vec4;
 	material: number;
+};
+
+type BumperMotion = {
+	entity: Entity;
+	basePosition: vec3;
+	maxXOffset: number;
+};
+
+type TriggerMotion = {
+	entity: Entity;
+	baseRotation: quat;
 };
 
 const loadButtonVisualizer = async (
@@ -49,15 +63,40 @@ const setButtonActive = async (
 	);
 };
 
-const rotateThumbstick = async (entity: Entity, axis: vec2): Promise<void> => {
+const rotateThumbstick = async (
+	entity: Entity,
+	axis: vec2,
+	flipXAxis: boolean,
+): Promise<void> => {
 	const absoluteRotation = quat.create();
 	quat.fromEuler(
 		absoluteRotation,
-		axis[1] * THUMBSTICK_MAX_ANGLE_DEGREES,
-		axis[0] * THUMBSTICK_MAX_ANGLE_DEGREES,
+		-axis[1] * THUMBSTICK_MAX_ANGLE_DEGREES,
+		(flipXAxis ? -axis[0] : axis[0]) * THUMBSTICK_MAX_ANGLE_DEGREES,
 		0,
 	);
 	await TransformManager.SetLocalRotation(entity, absoluteRotation);
+};
+
+const setBumperGrip = async (
+	bumper: BumperMotion,
+	value: number,
+): Promise<void> => {
+	const nextPosition = vec3.clone(bumper.basePosition);
+	nextPosition[0] += bumper.maxXOffset * value;
+	await TransformManager.SetLocalPosition(bumper.entity, nextPosition);
+};
+
+const setTriggerPull = async (
+	trigger: TriggerMotion,
+	value: number,
+): Promise<void> => {
+	const pullRotation = quat.create();
+	quat.fromEuler(pullRotation, TRIGGER_MAX_X_DEGREES * value, 0, 0);
+
+	const nextRotation = quat.create();
+	quat.multiply(nextRotation, trigger.baseRotation, pullRotation);
+	await TransformManager.SetLocalRotation(trigger.entity, nextRotation);
 };
 
 Project.FromBundle(projectBundle).Launch({
@@ -83,20 +122,65 @@ Project.FromBundle(projectBundle).Launch({
 			sceneGraph["@Right_Controller"]["@XRController_Thumbstick_Buttons001"][
 				"@ThumbStick001"
 			].entityId;
+		const leftBumperEntity = sceneGraph["@Left_Controller"]["@Bumper"].entityId;
+		const rightBumperEntity =
+			sceneGraph["@Right_Controller"]["@Bumper001"].entityId;
+		const leftTriggerEntity =
+			sceneGraph["@Left_Controller"]["@Trigger"].entityId;
+		const rightTriggerEntity =
+			sceneGraph["@Right_Controller"]["@Trigger001"].entityId;
 
-		const buttonMap = new Map<string, ButtonVisualizer>([
+		const [
+			leftBumperBasePosition,
+			rightBumperBasePosition,
+			leftTriggerBaseRotation,
+			rightTriggerBaseRotation,
+		] = await Promise.all([
+			TransformManager.GetLocalPosition(leftBumperEntity),
+			TransformManager.GetLocalPosition(rightBumperEntity),
+			TransformManager.GetLocalRotation(leftTriggerEntity),
+			TransformManager.GetLocalRotation(rightTriggerEntity),
+		]);
+
+		const bumperMap = new Map<string, BumperMotion>([
 			[
 				DevicePath.LEFT_GRIP,
-				await loadButtonVisualizer(
-					sceneGraph["@Left_Controller"]["@Bumper"].entityId,
-				),
+				{
+					entity: leftBumperEntity,
+					basePosition: leftBumperBasePosition,
+					maxXOffset: LEFT_BUMPER_MAX_X_OFFSET,
+				},
 			],
 			[
-				DevicePath.LEFT_TRIGGER,
-				await loadButtonVisualizer(
-					sceneGraph["@Left_Controller"]["@Trigger"].entityId,
-				),
+				DevicePath.RIGHT_GRIP,
+				{
+					entity: rightBumperEntity,
+					basePosition: rightBumperBasePosition,
+					maxXOffset: RIGHT_BUMPER_MAX_X_OFFSET,
+				},
 			],
+		]);
+
+		const triggerMap = new Map<string, TriggerMotion>([
+			[
+				DevicePath.LEFT_TRIGGER,
+				{
+					entity: leftTriggerEntity,
+					baseRotation: leftTriggerBaseRotation,
+				},
+			],
+			[
+				DevicePath.RIGHT_TRIGGER,
+				{
+					entity: rightTriggerEntity,
+					baseRotation: rightTriggerBaseRotation,
+				},
+			],
+		]);
+
+		const buttonMap = new Map<string, ButtonVisualizer>([
+			[DevicePath.LEFT_GRIP, await loadButtonVisualizer(leftBumperEntity)],
+			[DevicePath.LEFT_TRIGGER, await loadButtonVisualizer(leftTriggerEntity)],
 			[
 				DevicePath.LEFT_PRIMARY_2D_AXIS_BUTTON,
 				await loadButtonVisualizer(leftStickEntity),
@@ -117,17 +201,10 @@ Project.FromBundle(projectBundle).Launch({
 					].entityId,
 				),
 			],
-			[
-				DevicePath.RIGHT_GRIP,
-				await loadButtonVisualizer(
-					sceneGraph["@Right_Controller"]["@Bumper001"].entityId,
-				),
-			],
+			[DevicePath.RIGHT_GRIP, await loadButtonVisualizer(rightBumperEntity)],
 			[
 				DevicePath.RIGHT_TRIGGER,
-				await loadButtonVisualizer(
-					sceneGraph["@Right_Controller"]["@Trigger001"].entityId,
-				),
+				await loadButtonVisualizer(rightTriggerEntity),
 			],
 			[
 				DevicePath.RIGHT_PRIMARY_2D_AXIS_BUTTON,
@@ -153,19 +230,26 @@ Project.FromBundle(projectBundle).Launch({
 
 		const updateScalarInput = async (devicePath: string, value: number) => {
 			const button = buttonMap.get(devicePath);
-			if (!button) return;
+			const bumper = bumperMap.get(devicePath);
+			const trigger = triggerMap.get(devicePath);
 
-			await setButtonActive(button, value >= BUTTON_ACTIVE_THRESHOLD);
+			await Promise.all([
+				button
+					? setButtonActive(button, value >= BUTTON_ACTIVE_THRESHOLD)
+					: Promise.resolve(),
+				bumper ? setBumperGrip(bumper, value) : Promise.resolve(),
+				trigger ? setTriggerPull(trigger, value) : Promise.resolve(),
+			]);
 		};
 
 		const updateThumbstick = async (devicePath: string, value: vec2) => {
 			if (devicePath === DevicePath.LEFT_PRIMARY_2D_AXIS) {
-				await rotateThumbstick(leftStickEntity, value);
+				await rotateThumbstick(leftStickEntity, value, true);
 				return;
 			}
 
 			if (devicePath === DevicePath.RIGHT_PRIMARY_2D_AXIS) {
-				await rotateThumbstick(rightStickEntity, value);
+				await rotateThumbstick(rightStickEntity, value, false);
 			}
 		};
 
