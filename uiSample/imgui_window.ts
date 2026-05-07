4f2e1d9aa461fd6666e298f79992375cd4541819
zekailin00 <zekailin00@gmail.com>
2026-05-07 10:00:25 -0700

    refactoring imgui library

--- uiSample/imgui_window.ts
+++ uiSample/imgui_window.ts
@@ -1,28 +1,40 @@
-import { EntityManager, TransformManager } from "@adamasvr/sdk";
-import { vec3 } from "gl-matrix";
+import { readFile } from "node:fs/promises";
 import * as ImGui from "imgui-adamas";
 import * as ImGui_Impl from "./imgui_impl_adamas_node";
 
+const DEFAULT_FONT_SIZE_PX = 24;
+const DEFAULT_DISPLAY_WIDTH = 1280;
+const DEFAULT_DISPLAY_HEIGHT = 720;
+
 export async function CreateImGuiWindow({
 	ui,
-	uiInit,
+	initOptions,
 }: {
 	ui: (imgui: typeof ImGui, timestep: number) => void;
-	uiInit?: (imgui: typeof ImGui) => void | Promise<void>;
+	initOptions: ImGui_Impl.AdamasInitOptions;
 }) {
 	await ImGui.default();
 	ImGui.CHECKVERSION();
 	ImGui.CreateContext();
 
-	const uiEntity = await EntityManager.Create("ImGui Host");
-	await TransformManager.SetLocalScale(uiEntity, vec3.fromValues(0.64, 0.36, 1));
-	await TransformManager.SetLocalPosition(uiEntity, vec3.fromValues(0, 1.0, 0));
-
-	ImGui.GetIO().Fonts.AddFontDefault();
+	const io = ImGui.GetIO();
+	const robotoFont = await readFile("Roboto-VariableFont_wdth,wght.ttf");
+	io.FontDefault = io.Fonts.AddFontFromMemoryTTF(
+		Uint8Array.from(robotoFont).buffer,
+		DEFAULT_FONT_SIZE_PX,
+		null,
+		io.Fonts.GetGlyphRangesDefault(),
+	);
 	ImGui.StyleColorsDark();
-	ImGui_Impl.Init({ targetEntity: uiEntity, displayWidth: 1280, displayHeight: 720 });
 
-	await uiInit?.(ImGui);
+	if (initOptions.displayHeight == undefined) {
+		initOptions.displayHeight = DEFAULT_DISPLAY_HEIGHT;
+	}
+	if (initOptions.displayWidth == undefined) {
+		initOptions.displayWidth = DEFAULT_DISPLAY_WIDTH;
+	}
+
+	ImGui_Impl.Init(initOptions);
 
 	let lastFrameTime = Date.now();
 	return setInterval(() => {
@@ -33,8 +45,15 @@ export async function CreateImGuiWindow({
 		ImGui_Impl.NewFrame(timestep);
 		ImGui.NewFrame();
 
-		ImGui.SetNextWindowPos(new ImGui.Vec2(0, 0), ImGui.Cond.Always, new ImGui.Vec2(0, 0));
-		ImGui.SetNextWindowSize(new ImGui.Vec2(1280, 720), ImGui.Cond.Always);
+		ImGui.SetNextWindowPos(
+			new ImGui.Vec2(0, 0),
+			ImGui.Cond.Always,
+			new ImGui.Vec2(0, 0),
+		);
+		ImGui.SetNextWindowSize(
+			new ImGui.Vec2(initOptions.displayWidth, initOptions.displayHeight),
+			ImGui.Cond.Always,
+		);
 
 		ImGui.Begin(
 			"UI",
--- uiSample/index.ts
+++ uiSample/index.ts
@@ -1,13 +1,19 @@
-import { Project } from "@adamasvr/sdk";
+import { EntityManager, Project, TransformManager } from "@adamasvr/sdk";
 import { projectBundle } from "adamasvr:editor";
+import { vec3 } from "gl-matrix";
 import { CreateImGuiWindow } from "./imgui_window";
 
 Project.FromBundle(projectBundle).Launch({
 	OnSetup: async () => {
+		const uiEntity = await EntityManager.Create("ImGui Host");
+		await TransformManager.SetLocalScale(uiEntity, vec3.fromValues(0.64, 0.36, 1));
+		await TransformManager.SetLocalPosition(uiEntity, vec3.fromValues(0, 1.0, 0));
+
 		await CreateImGuiWindow({
-			uiInit: (imgui) => {
-				const io = imgui.GetIO();
-				io.FontGlobalScale = 1.25;
+			initOptions: {
+				targetEntity: uiEntity,
+				displayWidth: 1280,
+				displayHeight: 720,
 			},
 			ui: (imgui) => {
 				imgui.Text("ImGui UI wrapper");
