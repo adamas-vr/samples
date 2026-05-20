83a5bf8a720e919e53ea120a8ace79abf5d95305
zekailin00 <zekailin00@gmail.com>
2026-05-20 13:20:04 -0700

    update for new SDK version

--- camera/index.ts
+++ camera/index.ts
@@ -12,32 +12,29 @@ import {
 } from "@adamasvr/sdk";
 import { projectBundle } from "adamasvr:editor";
 
-Project.FromBundle(projectBundle).Launch({
-	OnSetup: async (project, sceneGraph) => {
-		if (sceneGraph === undefined) return;
-		const display = sceneGraph["@Display"]["@Preview"].entityId;
-		const camera = sceneGraph["@Camera"]["@Film"].entityId;
-		const capture = sceneGraph["@Display"]["@Capture"].entityId;
-		const button = sceneGraph["@Camera"].entityId;
+Project.FromBundle(projectBundle).Launch(async (sceneGraph, project) => {
+	if (sceneGraph === undefined) return;
+	const display = sceneGraph["@Display"]["@Preview"].entityId;
+	const camera = sceneGraph["@Camera"]["@Film"].entityId;
+	const capture = sceneGraph["@Display"]["@Capture"].entityId;
+	const button = sceneGraph["@Camera"].entityId;
 
-		const renderTexture = await TextureManager.CreateRenderTexture(900, 1200);
+	const renderTexture = await TextureManager.CreateRenderTexture(900, 1200);
 
-		CameraManager.SetRenderTexture(camera, renderTexture);
-		const previewMat = await RenderableManager.GetMaterial(display, 0);
-		MaterialManager.SetTexture(
-			previewMat,
-			MaterialProperty.BaseColorMap,
-			renderTexture,
-		);
+	CameraManager.SetRenderTexture(camera, renderTexture);
+	const previewMat = await RenderableManager.GetMaterial(display, 0);
+	MaterialManager.SetTexture(
+		previewMat,
+		MaterialProperty.BaseColorMap,
+		renderTexture,
+	);
 
-		const tex = await TextureManager.Create2D(1, 1, TextureFormat.RGBA32);
-		const captureMat = await RenderableManager.GetMaterial(capture, 0);
-		MaterialManager.SetTexture(captureMat, MaterialProperty.BaseColorMap, tex);
+	const tex = await TextureManager.Create2D(1, 1, TextureFormat.RGBA32);
+	const captureMat = await RenderableManager.GetMaterial(capture, 0);
+	MaterialManager.SetTexture(captureMat, MaterialProperty.BaseColorMap, tex);
 
-		GrabInteractableManager.AddActivatedCallback(button, async () => {
-			const result = await TextureManager.ReadbackJPGImage(renderTexture);
-			TextureManager.LoadImage(tex, result.data);
-		});
-	},
-	OnTick: (project, timestep) => {},
+	GrabInteractableManager.AddActivatedCallback(button, async () => {
+		const result = await TextureManager.ReadbackJPGImage(renderTexture);
+		TextureManager.LoadImage(tex, result.data);
+	});
 });
