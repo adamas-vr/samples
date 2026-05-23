1498a8bbdf5fe4eda3754c3747d251f29d93295b
zekailin00 <zekailin00@gmail.com>
2026-05-22 19:46:48 -0700

    network ui interaction and use public packages

--- uiSample/index.ts
+++ uiSample/index.ts
@@ -1,56 +1,67 @@
 import {
 	MaterialManager,
 	MaterialProperty,
+	Networking,
 	Project,
 	RenderableManager,
 	type Material,
 } from "@adamasvr/sdk";
 import { projectBundle } from "adamasvr:editor";
-import { CreateImGuiWindow } from "imgui-adamas";
+import { CreateImGuiWindow } from "@adamasvr/imgui";
 import { vec4 } from "gl-matrix";
 
-const cubeColor = { r: 0.2, g: 0.45, b: 1, a: 1 };
-let cubeMaterial: Material | null = null;
-let cubeColorUpdate = Promise.resolve();
+type CubeColor = { r: number; g: number; b: number; a: number };
 
-Project.FromBundle(projectBundle).Launch({
-	OnSetup: async (_, sceneGraph) => {
-		const cubeEntity = sceneGraph["@cube"].entityId;
-		cubeMaterial = await RenderableManager.GetMaterial(cubeEntity);
-		await MaterialManager.SetColor(
-			cubeMaterial,
-			MaterialProperty.BaseColor,
-			vec4.fromValues(cubeColor.r, cubeColor.g, cubeColor.b, cubeColor.a),
-		);
+const initialCubeColor: CubeColor = { r: 0.2, g: 0.45, b: 1, a: 1 };
 
-		await CreateImGuiWindow(
-			{
-				targetEntity: sceneGraph["@UI Panel"].entityId,
-				displayWidth: 600,
-				displayHeight: 400,
-			},
-			(imgui) => {
-				imgui.Text("Cube color");
-				imgui.Separator();
-				if (
-					imgui.ColorEdit4("Base color", cubeColor) &&
-					cubeMaterial !== null
-				) {
-					const nextColor = vec4.fromValues(
-						cubeColor.r,
-						cubeColor.g,
-						cubeColor.b,
-						cubeColor.a,
-					);
-					cubeColorUpdate = cubeColorUpdate.then(() =>
-						MaterialManager.SetColor(
-							cubeMaterial!,
-							MaterialProperty.BaseColor,
-							nextColor,
-						),
-					);
-				}
-			},
+Project.FromBundle(projectBundle).Launch(async (sceneGraph, project) => {
+	const cubeEntity = sceneGraph["@cube"].entityId;
+	const cubeMaterial = await RenderableManager.GetMaterial(cubeEntity);
+	const cubeColor: CubeColor = { ...initialCubeColor };
+	let cubeColorUpdate = Promise.resolve();
+
+	const applyCubeColor = (nextColor: CubeColor) => {
+		cubeColor.r = nextColor.r;
+		cubeColor.g = nextColor.g;
+		cubeColor.b = nextColor.b;
+		cubeColor.a = nextColor.a;
+
+		const targetMaterial: Material = cubeMaterial;
+		const colorVector = vec4.fromValues(
+			nextColor.r,
+			nextColor.g,
+			nextColor.b,
+			nextColor.a,
 		);
-	},
+		cubeColorUpdate = cubeColorUpdate.then(() =>
+			MaterialManager.SetColor(
+				targetMaterial,
+				MaterialProperty.BaseColor,
+				colorVector,
+			),
+		);
+	};
+
+	const sharedCubeColor = Networking.NewVariable<CubeColor>(
+		{ ...initialCubeColor },
+		applyCubeColor,
+	);
+
+	await CreateImGuiWindow(
+		project,
+		{
+			targetEntity: sceneGraph["@UI Panel"].entityId,
+			displayWidth: 600,
+			displayHeight: 400,
+		},
+		(imgui) => {
+			imgui.Text(`Framerate: ${imgui.GetIO().Framerate.toFixed(1)} FPS`);
+			imgui.Separator();
+			imgui.Text("Cube color");
+			imgui.Separator();
+			if (imgui.ColorEdit4("Base color", cubeColor)) {
+				sharedCubeColor.value = { ...cubeColor };
+			}
+		},
+	);
 });
